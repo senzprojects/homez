96a8d09bbd29d1d105b49422196ed84f39f70da8
eranga herath <erangaeb@gmail.com>
2015-11-01 18:41:20 +0530

    connect with remote senz service

---
+++ app/src/main/aidl/com/score/senz/ISenzService.aidl
@@ -0,0 +1,9 @@
+package com.score.senz;
+
+import com.score.senzc.pojos.Senz;
+
+
+interface ISenzService {
+    // send senz messages to service via this function
+    void send(in Senz senz);
+}
--- app/src/main/java/com/score/homez/HomeActivity.java
+++ app/src/main/java/com/score/homez/HomeActivity.java
@@ -1,14 +1,75 @@
 package com.score.homez;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.score.senz.ISenzService;
+import com.score.senzc.enums.SenzTypeEnum;
+import com.score.senzc.pojos.Senz;
+import com.score.senzc.pojos.User;
+
+import java.util.HashMap;
 
 public class HomeActivity extends Activity {
+
+    Button connect;
+
+    // service interface
+    private ISenzService senzService = null;
+
+    // service connection
+    private ServiceConnection senzServiceConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            Log.d("TAG", "Connected with senz service");
+            senzService = ISenzService.Stub.asInterface(service);
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            senzService = null;
+            Log.d("TAG", "Disconnected from senz service");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent();
+        intent.setClassName("com.score.senz", "com.score.senz.services.RemoteSenzService");
+        bindService(intent, senzServiceConnection, Context.BIND_AUTO_CREATE);
+
+        Log.d("TAG", "Bound service");
+
+        connect = (Button) findViewById(R.id.connect);
+        connect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    senzService.send(new Senz("Id", "signature", SenzTypeEnum.PUT, null, new User("id", "era"), new HashMap<String, String>()));
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(senzServiceConnection);
     }
 
 }
